94641c84a72e3e3804ebd559aa8c63a2b05971f0
Jack Tanner <jtanner0786@gmail.com>
2026-02-21 10:11:57 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-#include <string>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -13,14 +11,26 @@ int main() {
 
     string input;
     getline(cin, input);
+    stringstream ss(input);
+    vector<string> tokens;
+    string token;
+    
+    while (getline(ss, token, ' ')) {
+      tokens.push_back(token);
+    }
 
-    if (input == "exit") {
+    if (tokens[0] == "exit") {
       break;
+    } else if (tokens[0] == "echo") {
+      for (int i = 1; i < tokens.size(); i++) {
+        cout << tokens[i] << " ";
+      }
+      cout << endl;
+      continue;
     }
 
     cout << input << ": command not found" << endl;
   }
-  
 
   return 0;
 }
